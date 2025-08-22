07226fc0dc4e3bbe2f72cd9f2eefe0e4829b5c0b
worknshubh <worknshubh@gmail.com>
2025-08-22 19:45:26 +0530

    add error page for invalid routes

---
+++ screens/errorpage.jsx
@@ -0,0 +1,30 @@
+import { useNavigate } from "react-router-dom";
+
+function ErrorPage() {
+  const navigate = useNavigate();
+  return (
+    <>
+      <div className="h-lvh flex justify-center items-center flex-col">
+        <h2 className="text-6xl">Salonify</h2>
+        <h2 className="text-8xl inter-normal m-4">404</h2>
+        <h4 className="text-3xl inter-normal">NOT FOUND</h4>
+        <div className="mt-5">
+          <img
+            src="../src/assets/images/errorpageimage.png"
+            className="h-120"
+          />
+        </div>
+        <button
+          className="bg-[#eeeeee] inter-normal text-2xl py-3 px-6 rounded-2xl cursor-pointer hover:bg-[#e6e6e6]"
+          onClick={() => {
+            navigate("/");
+          }}
+        >
+          Go to Home
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default ErrorPage;
